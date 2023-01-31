5dbab891e2906f4e644fa8051239ab4ee71d4049
maicolm98 <logiudice9818@gmail.com>
2023-01-31 00:35:56 -0300

    segundo commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,16 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//alert("ok");
+
+	//var edad;
+	let nombre;
+	let nombreCompleto;// notacion lover camel case
+nombre = prompt("maicolm");
+//esto es prompt pero no tengo idea que es.
+// nombre = "maicolm" ; // asignacion de tring.
+
+//mostrar por alert.
+alert(nombre);
 }
 
